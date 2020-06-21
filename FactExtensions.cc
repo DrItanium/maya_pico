07a665ffb7edc2ff80ebefef00a83cf2c4122265
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-06-21 16:48:28 -0700

    Implement PutSlot for FactBuilder

--- FactExtensions.cc
+++ FactExtensions.cc
@@ -3,3 +3,10 @@
 //
 
 #include "FactExtensions.h"
+PutSlotError maya::FactBuilder::putSlot(const std::string &slotName, CLIPSInteger *value) noexcept { return FBPutSlotCLIPSInteger(_contents, slotName.c_str(), value); }
+PutSlotError maya::FactBuilder::putSlot(const std::string &slotName, CLIPSFloat *value) noexcept { return FBPutSlotCLIPSFloat(_contents, slotName.c_str(), value); }
+PutSlotError maya::FactBuilder::putSlot(const std::string &slotName, CLIPSLexeme *value) noexcept { return FBPutSlotCLIPSLexeme(_contents, slotName.c_str(), value); }
+PutSlotError maya::FactBuilder::putSlot(const std::string &slotName, CLIPSExternalAddress *value) noexcept { return FBPutSlotCLIPSExternalAddress(_contents, slotName.c_str(), value); }
+PutSlotError maya::FactBuilder::putSlot(const std::string &slotName, CLIPSValue *value) noexcept {
+    return FBPutSlot(_contents, slotName.c_str(), value);
+}
--- FactExtensions.h
+++ FactExtensions.h
@@ -18,19 +18,21 @@ namespace maya {
     public:
         FactBuilder(Environment& env, const std::string& name) : _env(env), _contents(CreateFactBuilder(&env, name.c_str())) { }
         ~FactBuilder() noexcept { ::FBDispose(_contents); }
-        Fact* assert() noexcept { return FBassert(_contents); }
+        Fact* assert() noexcept { return FBAssert(_contents); }
         void abort() noexcept { FBAbort(_contents); }
         FactBuilderError setDeftemplate(const std::string& templateName) noexcept { return FBSetDeftemplate(_contents, templateName.c_str()); }
-        PutSlotError putSlot(const std::string& slotName, CLIPSInteger*) noexcept;
-        PutSlotError putSlot(const std::string& slotName, int64_t) noexcept;
-        PutSlotError putSlot(const std::string& slotName, CLIPSFloat*) noexcept;
-        PutSlotError putSlot(const std::string& slotName, double) noexcept;
-        PutSlotError putSlot(const std::string& slotName, CLIPSValue*) noexcept;
-        PutSlotError putSlot(const std::string& slotName, CLIPSLexeme*) noexcept;
-        PutSlotError putSlot(const std::string& slotName, Fact*) noexcept;
-        PutSlotError putSlot(const std::string& slotName, Instance*) noexcept;
-        PutSlotError putSlot(const std::string& slotName, CLIPSExternalAddress *) noexcept;
-        PutSlotError putSlot(const std::string& slotName, Multifield*) noexcept;
+        PutSlotError putSlot(const std::string& slotName, CLIPSInteger* value) noexcept;
+        PutSlotError putSlot(const std::string& slotName, CLIPSFloat* value) noexcept;
+        PutSlotError putSlot(const std::string& slotName, CLIPSLexeme* value) noexcept;
+        PutSlotError putSlot(const std::string& slotName, CLIPSExternalAddress * value) noexcept;
+        PutSlotError putSlot(const std::string& slotName, CLIPSValue* value) noexcept;
+#define X(kind, type) PutSlotError putSlot(const std::string& slotName, type value) noexcept { return FBPutSlot ## kind (_contents, slotName.c_str(), value); }
+        X(Integer, int64_t);
+        X(Float, double);
+        X(Fact, Fact*);
+        X(Instance, Instance*);
+        X(Multifield, Multifield*);
+#undef X
         inline FactBuilderError error() noexcept { return FBError(&_env); }
 #define X(kind) \
     inline PutSlotError putSlot(const std::string& slotName, const std::string& value, TreatLexemeAs ## kind ) noexcept {  \
