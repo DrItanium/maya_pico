a69cad20c5dbe7de5d4248b2b5fb0f61db3f405a
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-06-21 18:03:18 -0700

    New simple FactView implementation

---
+++ FactView.h
@@ -0,0 +1,30 @@
+//
+// Created by jwscoggins on 6/21/20.
+//
+
+#ifndef MAYA_FACTVIEW_H
+#define MAYA_FACTVIEW_H
+extern "C" {
+#include "clips.h"
+}
+#include <optional>
+#include <string>
+namespace maya {
+    class FactView {
+    public:
+        explicit FactView(::fact &value) : _raw(value) {}
+        [[nodiscard]] constexpr auto getFactIndex() const noexcept { return _raw.factIndex; }
+        [[nodiscard]] constexpr auto getHashValue() const noexcept { return _raw.hashValue; }
+        [[nodiscard]] constexpr bool isGarbage() const noexcept { return _raw.garbage; }
+        std::optional<FactView> getPreviousFact() noexcept;
+        std::optional<FactView> getNextFact() noexcept;
+        std::optional<FactView> getPreviousTemplateFact() noexcept;
+        std::optional<FactView> getNextTemplateFact() noexcept;
+        /// @todo implement basis slots after implementing MultifieldView
+        /// @todo implement whichDeftemplate support
+        Multifield &getTheProposition() noexcept { return _raw.theProposition; }
+    private:
+        ::fact &_raw;
+    };
+} // end namespace maya
+#endif //MAYA_FACTVIEW_H
