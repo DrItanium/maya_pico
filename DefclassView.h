f065caa55282d196293ec26ded41b437357d49ec
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-07-08 14:58:57 -0700

    Introduce Defclass view

---
+++ DefclassView.h
@@ -0,0 +1,34 @@
+//
+// Created by jwscoggins on 7/8/20.
+//
+
+#ifndef MAYA_DEFCLASSVIEW_H
+#define MAYA_DEFCLASSVIEW_H
+extern "C" {
+#include "clips.h"
+}
+#include <optional>
+#include <string>
+namespace maya {
+    class DefclassView {
+    public:
+        explicit DefclassView(defclass& val) : _raw(val) { }
+        std::string getName() const noexcept { return DefclassName(&_raw); }
+        std::string getPPForm() const noexcept { return DefclassPPForm(&_raw); }
+        std::string getModuleName() const noexcept { return DefclassModule(&_raw); };
+        CLIPSLexeme* getNamePointer() const noexcept { return GetDefclassNamePointer(&_raw); }
+        [[nodiscard]] bool isAbstract() const noexcept { return ClassAbstractP(&_raw); }
+        [[nodiscard]] bool isReactive() const noexcept { return ClassReactiveP(&_raw); }
+        bool isSubclassOf(const DefclassView& other) const noexcept;
+        bool isSuperclassOf(const DefclassView& other) const noexcept;
+        bool slotExists(const std::string& slotName, bool) const noexcept;
+        bool slotWritable(const std::string& slotName) const noexcept;
+        bool slotInitable(const std::string& slotName) const noexcept;
+        bool slotPublic(const std::string& slotName) const noexcept;
+        bool slotDirectAccess(const std::string& slotName) const noexcept;
+    private:
+        defclass& _raw;
+    };
+}
+
+#endif //MAYA_DEFCLASSVIEW_H
