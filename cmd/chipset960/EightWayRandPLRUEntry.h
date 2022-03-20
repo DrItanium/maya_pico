c9737ff9db8b4e01778f1c93374b23846869c696
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-03-20 15:30:10 -0700

    The implementation of 8way rand plru from the management engine version of the sxchipset uses pointers.
    Replace pointers with direct storage elements. The use of pointers was a way to create a single contiguous block of memory for data transfer purposes on the grand central m4.
    
    We do not need it here.

--- cmd/chipset960/EightWayRandPLRUEntry.h
+++ cmd/chipset960/EightWayRandPLRUEntry.h
@@ -54,22 +54,22 @@ public:
     CacheEntry& getLine(const TaggedAddress& theAddress) noexcept {
         uint8_t targetIndex = 0xFF;
         for (uint8_t i = 0; i < NumberOfWays; ++i) {
-            if (ways_[i]->matches(theAddress)) {
+            if (ways_[i].matches(theAddress)) {
                 updateFlags(i);
-                return *ways_[i];
-            } else if ((targetIndex >= NumberOfWays) && !ways_[i]->isValid()) {
+                return ways_[i];
+            } else if ((targetIndex >= NumberOfWays) && !ways_[i].isValid()) {
                 targetIndex = i;
             }
         }
 
         auto index = (targetIndex < NumberOfWays) ? targetIndex : getLeastRecentlyUsed();
         updateFlags(index);
-        ways_[index]->reset(theAddress);
-        return *ways_[index];
+        ways_[index].reset(theAddress);
+        return ways_[index];
     }
     void clear() noexcept {
         for (auto& way : ways_) {
-            way->clear();
+            way.clear();
         }
         bits_ = 0;
     }
@@ -131,7 +131,7 @@ private:
     }
 private:
     // This is RandPLRU Tree so we need to organize things correctly, I'm going to try four groups of two
-    CacheEntry* ways_[NumberOfWays] = { nullptr };
+    CacheEntry ways_[NumberOfWays];
     uint8_t bits_ = 0;
 };
 
