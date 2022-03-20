b8fa508b53d492c0a218582ad3b519e7be043eb0
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-03-20 12:27:02 -0700

    Pull in code from SxChipset to start

---
+++ cmd/chipset960/CacheEntry.h
@@ -0,0 +1,200 @@
+/**
+ * @file
+ * Generic Cache Entry Class
+ * @copyright
+ * maya
+ * Copyright (c) 2012-2022, Joshua Scoggins
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef SXCHIPSET_CACHEENTRY_H
+#define SXCHIPSET_CACHEENTRY_H
+#include "TaggedCacheAddress.h"
+#include "i960SxChipset.h"
+/**
+ * @brief Describes a single cache line which holds onto a customizable number of words which are some multiple of 8 bit bytes
+ * @tparam numTagBits The number of bits in a given address devoted to the index of the cache line
+ * @tparam maxAddressBits How many bits the entire address is comprised of (maximum of 32)
+ * @tparam numLowestBits How many bits (and through some may bytes) describe the offset into this cache line (this determines how many bytes are stored in this line)
+ * @tparam T The type of the backing memory storage (SDCard, PSRAM, etc)
+ * @tparam useSpecificTypeSizes When true, use the smallest type for each field in the address type used by this cache line. When false, use uint32_t. Should only be false on ARM platforms
+ */
+template<byte numTagBits, byte maxAddressBits, byte numLowestBits, typename T, bool useSpecificTypeSizes = true>
+class CacheEntry final {
+public:
+    /**
+     * @brief Divides the bytes that make up this cache line to this type
+     */
+    using Word = SplitWord16;
+    /**
+     * @brief The number of bytes cached by this line
+     */
+    static constexpr size_t NumBytesCached = pow2(numLowestBits);
+    /**
+     * @brief The number of words cached by this line (NumBytesCached / sizeof(Word))
+     */
+    static constexpr size_t NumWordsCached = NumBytesCached / sizeof(Word);
+    /**
+     * @brief A bitmask of the word types
+     */
+    static constexpr byte CacheEntryMask = NumWordsCached - 1;
+    /**
+     * @brief The number of positions to shift the offset component by when computing the entry offset
+     */
+    static constexpr byte CacheEntryShiftAmount = numberOfBitsForCount(sizeof(Word));
+    static_assert(CacheEntryShiftAmount != 0, "Defined word type must be a power of 2");
+    static_assert(CacheEntryShiftAmount < numLowestBits, "The words that make up a cache line need to be smaller than the entire storage in the cache line itself");
+    /**
+     * @brief The address type used to describe addresses passed to this line
+     */
+    using TaggedAddress = ::TaggedAddress<numTagBits, maxAddressBits, numLowestBits, useSpecificTypeSizes>;
+    /**
+     * @brief The type of the offset component of the line address (the least significant bits of the address)
+     */
+    using OffsetType = typename TaggedAddress::LowerType ;
+    /**
+     * @brief The type of the key component of the line address (the most significant bits of the address)
+     */
+    using KeyType = typename TaggedAddress::RestType;
+    /**
+     * @brief The code that the line uses to signify it does not hold onto valid data
+     */
+    static constexpr OffsetType  InvalidCacheLineState = 0xFF;
+    /**
+     * @brief The code that the line uses to signify that it holds onto valid data which has not been modified compared to backing memory source
+     */
+    static constexpr OffsetType CleanCacheLineState = 0xFE;
+public:
+    /**
+     * @brief Clear the contents of the line out (committing the contents to backing store if dirty) and then populate the line with new data from the backing store.
+     * @param newTag The address that represents the base of the new line (offset will be zero)
+     */
+    void reset(TaggedAddress newTag) noexcept {
+        // no match so pull the data in from main memory
+        if (isDirty()) {
+            // we compute the overall range as we go through this stuff
+            byte end = ((highestUpdated_ - dirty_) + 1);
+            (void)T::write(TaggedAddress{key_, newTag.getTagIndex()}.getAddress() + (dirty_ * sizeof(Word)),
+                           reinterpret_cast<byte *>(data + dirty_),
+                           sizeof(Word) * end);
+        }
+        dirty_ = CleanCacheLineState;
+        highestUpdated_ = 0;
+        // since we have called reset, now align the new address internally
+        key_ = newTag.getRest();
+        // this is a _very_ expensive operation
+        (void)T::read(newTag.aligned().getAddress(), reinterpret_cast<byte*>(data), NumBytesCached);
+    }
+    /**
+     * @brief Clear the entry without saving what was previously in it, necessary if the memory was reused for a different purpose
+     */
+    void clear() noexcept {
+        // clear all flags
+        dirty_ = InvalidCacheLineState;
+        highestUpdated_ = 0;
+        key_ = 0;
+        for (auto& a : data) {
+            a.wholeValue_ = 0;
+        }
+    }
+    /**
+     * @brief Given a valid line, compare the cache keys to see if they are equal
+     * @param addr The address to compare with the key stored inside the line
+     * @return True if line is valid and the keys match
+     */
+    [[nodiscard]] constexpr bool matches(TaggedAddress addr) const noexcept { return isValid() && (addr.getRest() == key_); }
+    /**
+     * @brief Return the word at a given offset; NOTE THERE IS NO CHECKS TO MAKE SURE YOU DIDN'T GO OUT OF BOUNDS!
+     * @param offset The offset into the line in word form (it is not a byte offset)
+     * @return The word itself at the given position (a copy!)
+     */
+    [[nodiscard]] constexpr auto get(OffsetType offset) const noexcept { return data[offset].getWholeValue(); }
+    /**
+     * @brief Update a given word in the line and check it to see if the dirty bits should be updated as well
+     * @param offset The word offset into the line
+     * @param style Is this a Full 16-bit update, lower8 update, or upper8 update? Chipset halt on anything else
+     * @param value The new value to update the target word in the line with
+     */
+    [[gnu::always_inline]]
+    inline void set(OffsetType offset, LoadStoreStyle style, Word value) noexcept {
+        /// @todo add support for Words which are larger than bus width
+        // while unsafe, assume it is correct because we only get this from the ProcessorSerializer, perhaps directly grab it?
+        auto &target = data[offset];
+        if (auto oldValue = target.getWholeValue(); oldValue != value.getWholeValue()) {
+            switch (style) {
+                case LoadStoreStyle::Full16:
+                    target = value;
+                    break;
+                case LoadStoreStyle::Lower8:
+                    target.bytes[0] = value.bytes[0];
+                    break;
+                case LoadStoreStyle::Upper8:
+                    target.bytes[1] = value.bytes[1];
+                    break;
+                default:
+                    signalHaltState(F("BAD LOAD STORE STYLE FOR SETTING A CACHE LINE"));
+            }
+            // do a comparison at the end to see if we actually changed anything
+            // the idea is that if the values don't change don't mark the cache line as dirty again
+            // it may already be dirty but don't force the matter on any write
+            // we can get here if it is a lower or upper 8 bit write so oldValue != value.getWholeValue()
+            if (oldValue != target.getWholeValue()) {
+                // consumes more flash to do it this way but we only update ram when we have something to change
+                if (offset < dirty_) {
+                    dirty_ = offset;
+                }
+                // compute the highest updated entry, useful for computing an upper transfer range
+                if (offset > highestUpdated_) {
+                    highestUpdated_ = offset;
+                }
+            }
+        }
+    }
+    /**
+     * @brief Check to see if the given line is valid
+     * @return True if the line is not invalid
+     */
+    [[nodiscard]] constexpr bool isValid() const noexcept { return dirty_ != InvalidCacheLineState; }
+    /**
+     * @brief Check to see if any words in the line have been modified in a meaningful way
+     * @return True if any words in the line have been modified
+     */
+    [[nodiscard]] constexpr bool isDirty() const noexcept { return dirty_ < NumWordsCached; }
+    /**
+     * @brief Return true if the line has valid unmodified data compared to the backing store
+     * @return True if no words in the valid line have been meaningfully modified
+     */
+    [[nodiscard]] constexpr bool isClean() const noexcept { return dirty_ == CleanCacheLineState; }
+private:
+    Word data[NumWordsCached]; // 16 bytes
+    KeyType key_ = 0;
+    /**
+     * @brief Describes lowest dirty word in a valid cache line; also denotes if the cache line is valid or not
+     */
+    OffsetType dirty_ = InvalidCacheLineState;
+    /**
+     * @brief The highest updated word in the cache line
+     */
+    OffsetType highestUpdated_ = 0;
+};
+#endif //SXCHIPSET_CACHEENTRY_H
---
+++ cmd/chipset960/TaggedCacheAddress.h
@@ -0,0 +1,130 @@
+/**
+ * @file
+ * Generic Cache Entry Address Class
+ * @copyright
+ * maya
+ * Copyright (c) 2012-2022, Joshua Scoggins
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef SXCHIPSET_TAGGEDCACHEADDRESS_H
+#define SXCHIPSET_TAGGEDCACHEADDRESS_H
+//#include "MCUPlatform.h"
+#include <cstdint>
+/**
+ * @brief A wrapper around a 32-bit i960Sx address broken up into four components [unused, key, tag, offset] in order from most significant bit to least.
+ * The unused portion is the most significant bits of the value which are beyond the totalBits template parameter (it cannot be accessed and is only logically a part of it).
+ * The key is used to uniquely identify an cache line, it is primarily used when checking for cache line matches.
+ * The tag is the array index where a given cache line will be stored.
+ * The offset represents the starting position in the cache line for the given request.
+ * @tparam tagBits The number of tag bits
+ * @tparam totalBits The total number of bits that are used of the 32-bit number (default is 32)
+ * @tparam offsetBits The number of bits allocated to the cache line itself (default is 4)
+ * @tparam useSpecificTypeSizes When true use the smallest type that can hold onto each of the three important components. When false, use uint32_t for all types. On ARM processors, set this to false otherwise errors happen
+ */
+template<uint8_t tagBits, uint8_t totalBits = 32, uint8_t offsetBits = 4, bool useSpecificTypeSizes = true>
+union TaggedAddress {
+    /**
+     * @brief The number of bits allocated to the offset component
+     */
+    static constexpr auto NumLowestBits = offsetBits;
+    /**
+     * @brief The number of bits allocated to the tag component
+     */
+    static constexpr auto NumTagBits = tagBits;
+    /**
+     * @brief The number of bits allocated to the key component
+     */
+    static constexpr auto NumRestBits = totalBits - (NumTagBits + NumLowestBits);
+    /**
+     * @brief The number of bits of a 32-bit address used to compute cache addresses
+     */
+    static constexpr auto MaximumAddressSize = totalBits;
+    static_assert((NumLowestBits + NumTagBits + NumRestBits) == MaximumAddressSize, "Too many or too few bits for this given tagged address!");
+    /**
+     * @brief The data type for the tag component
+     */
+    using TagType = Address;
+    /**
+     * @brief The data type for the key component
+     */
+    using RestType = Address;
+    /**
+     * @brief The data type for the offset component
+     */
+    using LowerType = Address;
+    /**
+     * @brief Construct an address from a full 32-bit address
+     * @param value The full 32-bit address to decompose
+     */
+    constexpr explicit TaggedAddress(Address value = 0) noexcept : base(value) { }
+    /**
+     * @brief Convert a SplitWord32 into a tagged address
+     * @param value The splitword32 to convert
+     */
+    constexpr explicit TaggedAddress(const SplitWord32& value) noexcept : TaggedAddress(value.getWholeValue()) { }
+    /**
+     * @brief Construct an address from the three important components
+     */
+    constexpr explicit TaggedAddress(RestType key, TagType tag, LowerType offset = 0) noexcept : lowest(offset), tagIndex(tag), rest(key) { }
+    /**
+     * @brief Set the core 32-bit value to 0
+     */
+    void clear() noexcept { base = 0; }
+    /**
+     * @brief retrieve the complete 32-bit address
+     * @return The underlying full 32-bit address
+     */
+    [[nodiscard]] constexpr auto getAddress() const noexcept { return base; }
+    /**
+     * @brief Get the tag component of this address
+     * @return The tag component of this address as a standalone value
+     */
+    [[nodiscard]] constexpr auto getTagIndex() const noexcept { return tagIndex; }
+    /**
+     * @brief Get the offset component of this address
+     * @return The offset component of this address as a standalone value
+     */
+    [[nodiscard]] constexpr auto getOffset() const noexcept { return lowest; }
+    /**
+     * @brief Get the key component of this address
+     * @return The key component of this address as a standalone value
+     */
+    [[nodiscard]] constexpr auto getRest() const noexcept { return rest; }
+    /**
+     * @brief Construct a copy of this address with the offset set to zero
+     * @return A copy of address with the offset field set to zero
+     */
+    [[nodiscard]] constexpr TaggedAddress aligned() const noexcept {
+        return TaggedAddress{rest, tagIndex};
+    }
+private:
+    Address base;
+    struct {
+        LowerType lowest : NumLowestBits;
+        TagType tagIndex : NumTagBits;
+        RestType rest : NumRestBits;
+    };
+    byte bytes_[sizeof(Address)];
+};
+#endif //SXCHIPSET_TAGGEDCACHEADDRESS_H
