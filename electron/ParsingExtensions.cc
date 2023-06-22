621549a23e175bb89cfabee8097187387ed3e200
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-06-21 19:59:24 -0700

    Fully implemented next-token

--- electron/ParsingExtensions.cc
+++ electron/ParsingExtensions.cc
@@ -24,6 +24,7 @@
 
 #include "electron/ParsingExtensions.h"
 #include "electron/Environment.h"
+#include "electron/MultifieldBuilder.h"
 #include "fs/path.h"
 #include <string>
 extern "C" {
@@ -60,6 +61,127 @@ nextTokenFunction(UDF_ARGS__) {
         out->lexemeValue = theEnv.createString("*** READ ERROR ***");
     }
     auto theToken = theEnv.getToken(logicalName);
+    auto& rtrData = theEnv.routerData();
+    rtrData.CommandBufferInputCount = 0;
+    rtrData.AwaitingInput = false;
+    switch(theToken.tknType) {
+        case TokenType::SYMBOL_TOKEN:
+            out->lexemeValue = theToken.lexemeValue;
+            break;
+        case TokenType::STRING_TOKEN:
+            out->lexemeValue = theToken.lexemeValue;
+            break;
+        case TokenType::INSTANCE_NAME_TOKEN:
+            out->lexemeValue = theToken.lexemeValue;
+            break;
+        case TokenType::FLOAT_TOKEN:
+            out->floatValue = theToken.floatValue;
+            break;
+        case TokenType::INTEGER_TOKEN:
+            out->integerValue = theToken.integerValue;
+            break;
+        case TokenType::LEFT_PARENTHESIS_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("LEFT_PARENTHESIS", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::RIGHT_PARENTHESIS_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("RIGHT_PARENTHESIS", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::SF_VARIABLE_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("SF_VARIABLE", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::MF_VARIABLE_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("MF_VARIABLE", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::GBL_VARIABLE_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("GBL_VARIABLE", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::SF_WILDCARD_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("SF_WILDCARD", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::MF_WILDCARD_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("MF_WILDCARD", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::MF_GBL_VARIABLE_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("MF_GBL_WILDCARD", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::NOT_CONSTRAINT_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("NOT_CONSTRAINT", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::AND_CONSTRAINT_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("AND_CONSTRAINT", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::OR_CONSTRAINT_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("OR_CONSTRAINT", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::STOP_TOKEN:
+        {
+            MultifieldBuilder bldr(theEnv, 2);
+            bldr.append("STOP", TreatLexemeAsSymbol{});
+            bldr.append(theToken.printForm, TreatLexemeAsSymbol{});
+            out->multifieldValue = bldr.create();
+            break;
+        }
+        case TokenType::UNKNOWN_VALUE_TOKEN:
+        default:
+            out->lexemeValue = theEnv.createString("*** READ ERROR ***");
+            break;
+    }
+
 }
 
 } // end namespace Electron
